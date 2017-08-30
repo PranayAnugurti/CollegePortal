f9fba97206c706a383cda5b4b18e52002550128e
DAN <praneethkumarred17@gmail.com>
2017-08-30 20:29:36 +0530

    Academic form is created variable need to be updated

--- app/src/main/java/com/praneethcorporation/collegeportal/Tab2.java
+++ app/src/main/java/com/praneethcorporation/collegeportal/Tab2.java
@@ -1,22 +1,33 @@
 package com.praneethcorporation.collegeportal;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by user on 8/29/2017.
  */
 
 public class Tab2 extends Fragment {
-
+Button ed;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab2, container, false);
         Context c = getActivity().getApplicationContext();
+   ed = (Button)view.findViewById(R.id.editButton2);
+        ed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),Tab2Form.class);
+                startActivity(intent);
+            }
+        });
         return view;
+
     }
 }
---
+++ app/src/main/java/com/praneethcorporation/collegeportal/Tab2Form.java
@@ -0,0 +1,13 @@
+package com.praneethcorporation.collegeportal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tab2Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab2form);
+    }
+}
