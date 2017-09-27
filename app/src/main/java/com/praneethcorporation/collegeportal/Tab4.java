dd7a91e6d1c35f5f4e47c8fad253b5eb4d6f060d
DAN <praneethkumarred17@gmail.com>
2017-09-28 04:06:26 +0530

    Retrieving Image from SDCARD

--- app/src/main/java/com/praneethcorporation/collegeportal/Tab4.java
+++ app/src/main/java/com/praneethcorporation/collegeportal/Tab4.java
@@ -1,21 +1,75 @@
 package com.praneethcorporation.collegeportal;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by user on 8/29/2017.
  */
 
 public class Tab4 extends Fragment {
+    Button UploadBn, ChooseImg;
+    ImageView imgView;
+    private final int IMG_REQUEST = 1;
+    private Bitmap bitmap;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab4, container, false);
         Context c = getActivity().getApplicationContext();
+
+        ChooseImg = (Button)view.findViewById(R.id.chooseImage);
+        UploadBn = (Button) view.findViewById(R.id.uploadBn);
+        imgView = (ImageView)view.findViewById(R.id.imageView);
+        ChooseImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImageFromGallery();
+            }
+        });
+
+
+
         return view;
+    }
+
+
+    public void selectImageFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMG_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMG_REQUEST && resultCode == RESULT_OK && data != null) {
+            Uri path = data.getData();
+            try {
+
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), path);
+                imgView.setImageBitmap(bitmap);
+                imgView.setVisibility(View.VISIBLE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
