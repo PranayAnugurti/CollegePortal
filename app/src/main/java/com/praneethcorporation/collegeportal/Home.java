c33f5004510ff6113005090faf3df82477d1b805
DAN <praneethkumarred17@gmail.com>
2017-10-06 11:38:08 +0530

    Conflict of placement statistics not opening resolved

--- app/src/main/java/com/praneethcorporation/collegeportal/Home.java
+++ app/src/main/java/com/praneethcorporation/collegeportal/Home.java
@@ -26,8 +26,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
 import com.squareup.picasso.Picasso;
+
 import de.hdodenhof.circleimageview.CircleImageView;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -40,6 +43,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -48,122 +52,123 @@ import org.w3c.dom.Text;
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-  String reg_no;
-  UserInfo info;
-  CircleImageView imageView;
-  TextView nameView;
-  TextView streamView;
-  TextView branchView;
-  TextView regView;
-  Button viewProfileButton;
-  String JSON_STRING;
-  ProgressBar loadingIndicator;
+    String reg_no;
+    UserInfo info;
+    CircleImageView imageView;
+    TextView nameView;
+    TextView streamView;
+    TextView branchView;
+    TextView regView;
+    Button viewProfileButton;
+    String JSON_STRING;
+    ProgressBar loadingIndicator;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_home);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    //Profile Activity access
+        //Profile Activity access
 
-    BackgroundTask task = new BackgroundTask(this);
-    task.execute();
-    imageView = (CircleImageView) findViewById(R.id.profilePicImageView);
-    nameView = (TextView) findViewById(R.id.nameTextView);
-    streamView = (TextView) findViewById(R.id.streamTextView);
-    branchView = (TextView) findViewById(R.id.branchTextView);
-    regView = (TextView) findViewById(R.id.regTextView);
-    viewProfileButton = (Button) findViewById(R.id.viewProfileButton);
-    loadingIndicator = (ProgressBar) findViewById(R.id.loading_indicator);
+        BackgroundTask task = new BackgroundTask(this);
+        task.execute();
+        imageView = (CircleImageView) findViewById(R.id.profilePicImageView);
+        nameView = (TextView) findViewById(R.id.nameTextView);
+        streamView = (TextView) findViewById(R.id.streamTextView);
+        branchView = (TextView) findViewById(R.id.branchTextView);
+        regView = (TextView) findViewById(R.id.regTextView);
+        viewProfileButton = (Button) findViewById(R.id.viewProfileButton);
+        loadingIndicator = (ProgressBar) findViewById(R.id.loading_indicator);
 
-    viewProfileButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Intent intent = new Intent(getApplicationContext(), Profile.class);
-        intent.putExtra("reg_no", reg_no);
-        intent.putExtra("image", info.image_server_link);
-        intent.putExtra("pdf", info.pdf_server_link);
-        startActivity(intent);
-      }
-    });
+        viewProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Profile.class);
+                intent.putExtra("reg_no", reg_no);
+                intent.putExtra("image", info.image_server_link);
+                intent.putExtra("pdf", info.pdf_server_link);
+                startActivity(intent);
+            }
+        });
 
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-    drawer.setDrawerListener(toggle);
-    toggle.syncState();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-    navigationView.setNavigationItemSelectedListener(this);
-  }
-
-  @Override
-  public void onBackPressed() {
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-    if (drawer.isDrawerOpen(GravityCompat.START)) {
-      drawer.closeDrawer(GravityCompat.START);
-    } else {
-      super.onBackPressed();
-    }
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.home, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    return super.onOptionsItemSelected(item);
-  }
-
-  @SuppressWarnings("StatementWithEmptyBody")
-  @Override
-  public boolean onNavigationItemSelected(MenuItem item) {
-    // Handle navigation view item clicks here.
-    int id = item.getItemId();
-
-    if (id == R.id.nav_currentOpening) {
-      Intent intent = new Intent(Home.this, CurrentOpenings.class);
-      startActivity(intent);
-    } else if (id == R.id.nav_registeredCompanies) {
-      Intent intent = new Intent(Home.this, RegisterCompanies.class);
-      startActivity(intent);
-    } else if (id == R.id.nav_addInterviewExperience) {
-      if (id == R.id.nav_currentOpening) {
-        Intent intent = new Intent(Home.this, CurrentOpenings.class);
-        startActivity(intent);
-      } else if (id == R.id.nav_registeredCompanies) {
-        Intent intent = new Intent(Home.this, RegisterCompanies.class);
-        startActivity(intent);
-      } else if (id == R.id.nav_addInterviewExperience) {
-
-      } else if (id == R.id.nav_interviewExperinces) {
-
-      } else if (id == R.id.nav_place_stats) {
-        Intent intent = new Intent(Home.this, PlaceMentStatistics.class);
-        startActivity(intent);
-      }
-
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-    drawer.closeDrawer(GravityCompat.START);
-    return true;
-  }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_currentOpening) {
+            Intent intent = new Intent(Home.this, CurrentOpenings.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_registeredCompanies) {
+            Intent intent = new Intent(Home.this, RegisterCompanies.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_addInterviewExperience) {
+
+        } else if (id == R.id.nav_currentOpening) {
+            Intent intent = new Intent(Home.this, CurrentOpenings.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_registeredCompanies) {
+            Intent intent = new Intent(Home.this, RegisterCompanies.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_addInterviewExperience) {
+
+        } else if (id == R.id.nav_interviewExperinces) {
+
+        } else if (id == R.id.nav_place_stats) {
+            Intent intent = new Intent(getApplicationContext(), PlaceMentStatistics.class);
+            startActivity(intent);
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     public class BackgroundTask extends AsyncTask<Void, Void, String> {
 
@@ -242,18 +247,18 @@ public class Home extends AppCompatActivity
                 JSONObject base_json = new JSONObject(result);
                 Log.d("O_MY", base_json.toString());
 
-        JSONObject a = base_json.getJSONObject("response");
+                JSONObject a = base_json.getJSONObject("response");
 
-        info = new UserInfo(a.getString("reg_no"), a.getString("name"), a.getString("user_pass"),
-                a.getString("image"),a.getString("imageServerLink"),a.getString("pdfServerLink"), a.getString("course"), a.getString("branch"), a.getString("dob"),
-                a.getString("email"), a.getString("skype"), a.getString("linkedin"),
-                a.getString("gender"),
-                a.getString("category"), a.getString("phd"), a.getString("residential_status"),
-                a.getString("guardian"),
-                a.getString("present_address"), a.getString("permanent_address"),
-                a.getString("marital_status"), a.getString("state"), a.getString("country"));
-        Log.d("O_MY_PAPPI", a.getString("reg_no") + a.getString("email"));
-        String s = info.image_url;
+                info = new UserInfo(a.getString("reg_no"), a.getString("name"), a.getString("user_pass"),
+                        a.getString("image"), a.getString("imageServerLink"), a.getString("pdfServerLink"), a.getString("course"), a.getString("branch"), a.getString("dob"),
+                        a.getString("email"), a.getString("skype"), a.getString("linkedin"),
+                        a.getString("gender"),
+                        a.getString("category"), a.getString("phd"), a.getString("residential_status"),
+                        a.getString("guardian"),
+                        a.getString("present_address"), a.getString("permanent_address"),
+                        a.getString("marital_status"), a.getString("state"), a.getString("country"));
+                Log.d("O_MY_PAPPI", a.getString("reg_no") + a.getString("email"));
+                String s = info.image_url;
 
                 //you have to get the part of the link 0B9nFwumYtUw9Q05WNlhlM2lqNzQ
                 String[] p = s.split("/");
